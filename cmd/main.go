6025e74ea9c59dbe54f6940c8dfce46d2379c6a5
AVD\BOBA <asoyants-vova@yandex.ru>
2021-07-31 01:33:08 +0300

    Adding one line inside func main().

--- cmd/main.go
+++ cmd/main.go
@@ -7,5 +7,8 @@ import (
 
 func main() {
 	fmt.Printf("Starting func main() at:\t%v\n", time.Now())
+
+	fmt.Println("Performing some activity inside func main()...")
+
 	defer fmt.Printf("Finishing func main() at:\t%v\n", time.Now())
 }

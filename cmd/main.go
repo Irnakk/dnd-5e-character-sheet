1cc282822014789310a713068fb379b9920ec7ed
AVD\BOBA <asoyants-vova@yandex.ru>
2021-07-31 01:15:29 +0300

    In formatted print, added time of entering and leaving func main().

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-	fmt.Println("Starting func main()...")
-	defer fmt.Println("Finishing func main()...")
+	fmt.Printf("Starting func main() at:\t%v\n", time.Now())
+	defer fmt.Printf("Finishing func main() at:\t%v\n", time.Now())
 }

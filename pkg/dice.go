b024d054411b4f715dcefae537b2575a0da05c90
AVD\BOBA <asoyants-vova@yandex.ru>
2021-07-31 12:42:08 +0300

    Added and tested diceVerbose(number, value int) function; changed Dice() to dice(). Coverage 95.0% because of an excessive error check.

--- pkg/dice.go
+++ pkg/dice.go
@@ -6,7 +6,7 @@ import (
 )
 
 var (
-	dice = map[int]struct{}{
+	dice_types = map[int]struct{}{
 		4:   {},
 		6:   {},
 		8:   {},
@@ -17,8 +17,8 @@ var (
 	}
 )
 
-func Dice(number, value int) (int, error) {
-	if _, found := dice[value]; !found {
+func dice(number, value int) (int, error) {
+	if _, found := dice_types[value]; !found {
 		return 0, errors.New("no such die")
 	}
 
@@ -34,4 +34,28 @@ func Dice(number, value int) (int, error) {
 	}
 
 	return sum, nil
+}
+
+func diceVerbose(number, value int) ([]int, error) {
+	if _, found := dice_types[value]; !found {
+		return []int{}, errors.New("no such die")
+	}
+
+	if number < 1 {
+		return []int{}, errors.New("invalid number of rolls")
+	}
+
+	result := make([]int, number)
+
+	for i := 0; i < number; i++ {
+		die, err := dice(1, value)
+
+		if err != nil {
+			return []int{}, errors.New("error in rolling a single die")
+		}
+
+		result = append(result, die)
+	}
+
+	return result, nil
 }
--- pkg/dice_test.go
+++ pkg/dice_test.go
@@ -5,29 +5,59 @@ import "testing"
 func TestDice(t *testing.T) {
 
 	// test for invalid number of rolls
-	result, err := Dice(-1, 8)
+	result_inv_num, err_inv_num := dice(-1, 8)
 
-	if result != 0 || err == nil {
-		t.Errorf("Dice(-1, 8) failed; expected %v, got %v", "error", result)
+	if result_inv_num != 0 || err_inv_num == nil {
+		t.Errorf("dice(-1, 8) failed; expected %v, got %v", "error", result_inv_num)
 	} else {
-		t.Logf("Dice(-1, 8) success; expected %v, got %v", "error", err)
+		t.Logf("dice(-1, 8) success; expected %v, got %v", "error", err_inv_num)
 	}
 
 	// test for an invaild die
-	result, err = Dice(2, 7)
+	result_inv_die, err_inv_die := dice(2, 7)
 
-	if result != 0 || err == nil {
-		t.Errorf("Dice(2, 7) failed; expected %v, got %v", "error", result)
+	if result_inv_die != 0 || err_inv_die == nil {
+		t.Errorf("dice(2, 7) failed; expected %v, got %v", "error", result_inv_die)
 	} else {
-		t.Logf("Dice(2, 7) success; expected %v, got %v", "error", err)
+		t.Logf("dice(2, 7) success; expected %v, got %v", "error", err_inv_die)
 	}
 
 	// test for valid arguments
-	result, err = Dice(4, 6)
+	result_valid, err_valid := dice(4, 6)
+
+	if err_valid != nil {
+		t.Errorf("dice(4, 6) failed; expected %v, got %v", "4d6", err_valid)
+	} else {
+		t.Logf("dice(4, 6) success; expected %v, got %v", "4d6", result_valid)
+	}
+}
+
+func TestDiceVerbose(t *testing.T) {
+
+	// test for invalid number of rolls
+	result, err := diceVerbose(0, 20)
+
+	if len(result) != 0 || err == nil {
+		t.Errorf("diceVerbose(0, 20) failed; expected %v, got %v", "error", result)
+	} else {
+		t.Logf("diceVerbose(0, 20) success; expected %v, got %v", "error", err)
+	}
+
+	// test for an invaild die
+	result, err = diceVerbose(1, 3)
+
+	if len(result) != 0 || err == nil {
+		t.Errorf("diceVerbose(1, 3) failed; expected %v, got %v", "error", result)
+	} else {
+		t.Logf("diceVerbose(1, 3) success; expected %v, got %v", "error", err)
+	}
+
+	// test for valid arguments
+	result, err = diceVerbose(4, 6)
 
 	if err != nil {
-		t.Errorf("Dice(4, 6) failed; expected %v, got %v", "4d6", err)
+		t.Errorf("diceVerbose(4, 6) failed; expected %v, got %v", "4d6", err)
 	} else {
-		t.Logf("Dice(4, 6) success; expected %v, got %v", "4d6", result)
+		t.Logf("diceVerbose(4, 6) success; expected %v, got %v", "4d6", result)
 	}
 }

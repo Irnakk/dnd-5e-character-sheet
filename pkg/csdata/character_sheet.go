e9c072d2bf044d90e668cfa924a1a84034a7060b
AVD\BOBA <asoyants-vova@yandex.ru>
2021-11-01 21:41:34 +0300

    D5CS-47 added WriteToDB() for CharacterSheet; still need to track skills there

--- pkg/csdata/character_sheet.go
+++ pkg/csdata/character_sheet.go
@@ -319,7 +319,8 @@ func (sheet *CharacterSheet) ReadFromDB(id int) error {
 	}
 	defer db.Close()
 
-	query := fmt.Sprintf(`SELECT "current_level",
+	query := fmt.Sprintf(`
+	SELECT "current_level",
 	"str_base", "dex_base", "con_base",
 	"int_base", "wis_base", "cha_base",
 	"str_bonus", "dex_bonus", "con_bonus",
@@ -327,7 +328,7 @@ func (sheet *CharacterSheet) ReadFromDB(id int) error {
 	"st_str_prof", "st_dex_prof", "st_con_prof",
 	"st_int_prof", "st_wis_prof", "st_cha_prof"
 	FROM "sheets"
-	WHERE "sheet_id" = %d`, id)
+	WHERE "sheet_id" = %d;`, id)
 
 	result := db.QueryRow(query)
 
@@ -379,6 +380,68 @@ func (sheet *CharacterSheet) ReadFromFile(name string) error {
 	} else {
 		fmt.Printf("File does not exist:\t%v\n", err)
 		return err
+	}
+
+	return nil
+}
+
+func (sheet *CharacterSheet) WriteToDB(id int) error {
+	file_content, err := ioutil.ReadFile("C:/d5cs/db_login")
+	if err != nil {
+		fmt.Printf("Error in opening login file:\t%v\n", err)
+		return err
+	}
+
+	psqlconn := string(file_content)
+
+	db, err := sql.Open("postgres", psqlconn)
+	if err != nil {
+		fmt.Printf("Error in opening DB:\t%v\n", err)
+		return err
+	}
+	defer db.Close()
+
+	query := `
+	UPDATE "sheets"
+	SET "current_level" = $1,
+
+	"str_base" = $2, "dex_base" = $3, "con_base" = $4,
+	"int_base" = $5, "wis_base" = $6, "cha_base" = $7,
+
+	"str_bonus" = $8, "dex_bonus" = $9, "con_bonus" = $10,
+	"int_bonus" = $11, "wis_bonus" = $12, "cha_bonus" = $13,
+
+	"st_str_prof" = $14, "st_dex_prof" = $15, "st_con_prof" = $16,
+	"st_int_prof" = $17, "st_wis_prof" = $18, "st_cha_prof" = $19
+
+	WHERE "sheet_id" = $20;
+	`
+
+	result, err := db.Exec(query,
+		sheet.Level,
+		sheet.StatsBase.Strength, sheet.StatsBase.Dexterity, sheet.StatsBase.Constitution,
+		sheet.StatsBase.Intelligence, sheet.StatsBase.Wisdom, sheet.StatsBase.Charisma,
+
+		sheet.StatsBonuses.Strength, sheet.StatsBonuses.Dexterity, sheet.StatsBonuses.Constitution,
+		sheet.StatsBonuses.Intelligence, sheet.StatsBonuses.Wisdom, sheet.StatsBonuses.Charisma,
+
+		sheet.STProficiency.Strength, sheet.STProficiency.Dexterity, sheet.STProficiency.Constitution,
+		sheet.STProficiency.Intelligence, sheet.STProficiency.Wisdom, sheet.STProficiency.Charisma,
+
+		id)
+	if err != nil {
+		fmt.Printf("Error in updating DB:\t%v\n", err)
+		return err
+	}
+
+	n_rows, err := result.RowsAffected()
+	if err != nil {
+		fmt.Printf("Error in RowsAffected():\t%v\n", err)
+		return err
+	}
+	fmt.Printf("Number of affected rows:\t%v\n", n_rows)
+	if n_rows == 0 {
+		fmt.Printf("No rows affected; could not find row with id=%d\n", id)
 	}
 
 	return nil
